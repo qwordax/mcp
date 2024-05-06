29a52dc660ae0106dfdc8f30306206ffa2056c5c
Anatoly Nadolsky <a.nadolsky@proton.me>
2024-05-06 15:45:30 +0300

    `feat:` update `top_chatter`

--- top_chatter.vhd
+++ top_chatter.vhd
@@ -3,6 +3,9 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity top_chatter is
+generic (
+    width: natural
+);
 port (
     d:  in  std_logic;
     cl: in  std_logic;
@@ -13,12 +16,12 @@ end entity top_chatter;
 architecture rtl of top_chatter is
 begin
     process (d, cl) is
-        variable v_tmp: integer range 0 to 5_000_000;
+        variable v_tmp: natural range 0 to 2 ** width - 1;
     begin
         if cl'event and cl = '1' then
-            if d = '0' and v_tmp > 0 then
+            if d = '0' and v_tmp > v_tmp'low then
                 v_tmp := v_tmp - 1;
-            elsif d = '1' and v_tmp < 5_000_000 then
+            elsif d = '1' and v_tmp < v_tmp'high then
                 v_tmp := v_tmp + 1;
             end if;
 
