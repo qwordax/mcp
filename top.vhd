5f4c46f63496c2c77961ebce47ba3eaef72d0c7a
Anatoly Nadolsky <a.nadolsky@proton.me>
2024-05-07 13:56:47 +0300

    `feat:` implement `rs` and `rd`

--- top.vhd
+++ top.vhd
@@ -7,13 +7,13 @@ port (
     key:    in  std_logic_vector(3 downto 0);
     sw:     in  std_logic_vector(7 downto 0);
     cl:     in  std_logic;
-    led:    out std_logic_vector(11 downto 0);
-    sd_sg:  out std_logic_vector(7 downto 0);
-    sd_cs:  out std_logic_vector(7 downto 0);
-    lcd_rs: out std_logic;
-    lcd_rw: out std_logic;
-    lcd_en: out std_logic;
-    lcd_d:  out std_logic_vector(7 downto 0)
+    led:    out std_logic_vector(11 downto 0)
+--    sd_sg:  out std_logic_vector(7 downto 0);
+--    sd_cs:  out std_logic_vector(7 downto 0);
+--    lcd_rs: out std_logic;
+--    lcd_rw: out std_logic;
+--    lcd_en: out std_logic;
+--    lcd_d:  out std_logic_vector(7 downto 0)
 );
 end entity top;
 
@@ -39,6 +39,18 @@ architecture rtl of top is
     );
     end component c_div;
 
+    component c_ctr is
+    generic (
+        width: natural
+    );
+    port (
+        r:  in  std_logic;
+        cl: in  std_logic;
+        en: in  std_logic;
+        q:  out std_logic_vector(width - 1 downto 0)
+    );
+    end component c_ctr;
+
     component top_ctrl is
     port (
         u:  in  std_logic;
@@ -51,12 +63,14 @@ architecture rtl of top is
     signal s_key: std_logic_vector(3 downto 0);
     signal s_sw:  std_logic_vector(7 downto 0);
 
-    signal cl_ctrl: std_logic;
+    signal s_cldiv: std_logic;
 
-    signal mcp_op: std_logic_vector(5 downto 0);
+    signal s_op: std_logic_vector(5 downto 0);
+    signal s_rs: std_logic_vector(2 downto 0);
+    signal s_rd: std_logic_vector(2 downto 0);
 begin
-    key_ch0: for i in 0 to 3 generate
-        chatter0: c_chatter
+    key_ch: for i in 0 to 3 generate
+        chatter: component c_chatter
         generic map (
             width => 20
         )
@@ -65,10 +79,10 @@ begin
             cl => cl,
             q  => s_key(i)
         );
-    end generate key_ch0;
+    end generate key_ch;
 
-    sw_ch0: for i in 0 to 7 generate
-        chatter0: c_chatter
+    sw_ch: for i in 0 to 7 generate
+        chatter: component c_chatter
         generic map (
             width => 4
         )
@@ -77,22 +91,48 @@ begin
             cl => cl,
             q  => s_sw(i)
         );
-    end generate sw_ch0;
+    end generate sw_ch;
 
-    div0: c_div
+    div: component c_div
     generic map (
         divide => 12_500_000
     )
     port map (
         d => cl,
-        q => cl_ctrl
+        q => s_cldiv
     );
 
-    ctrl0: top_ctrl
+    ctrl: component top_ctrl
     port map (
         u  => s_key(0),
         d  => s_key(1),
-        cl => cl_ctrl,
-        q  => mcp_op
+        cl => s_cldiv,
+        q  => s_op
     );
+
+    rs: component c_ctr
+    generic map (
+        width => 3
+    )
+    port map (
+        r  => '0',
+        cl => s_cldiv,
+        en => s_key(2),
+        q  => s_rs
+    );
+
+    rd: component c_ctr
+    generic map (
+        width => 3
+    )
+    port map (
+        r  => '0',
+        cl => s_cldiv,
+        en => s_key(3),
+        q  => s_rd
+    );
+
+    led(5 downto 0)  <= not s_op;
+    led(8 downto 6)  <= not s_rs;
+    led(11 downto 9) <= not s_rd;
 end architecture rtl;
