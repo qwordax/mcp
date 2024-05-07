b40d8e9b263ad279013300fdb594219ad056a087
Anatoly Nadolsky <a.nadolsky@proton.me>
2024-05-07 12:43:42 +0300

    `feat:` add `c_chatter` to `key` and `sw`

--- top.vhd
+++ top.vhd
@@ -18,6 +18,41 @@ port (
 end entity top;
 
 architecture rtl of top is
-begin
+    component c_chatter is
+    generic (
+        width: natural
+    );
+    port (
+        d:  in  std_logic;
+        cl: in  std_logic;
+        q:  out std_logic
+    );
+    end component c_chatter;
 
+    signal s_key: std_logic_vector(3 downto 0);
+    signal s_sw:  std_logic_vector(7 downto 0);
+begin
+    key_ch0: for i in 0 to 3 generate
+        chatter0: c_chatter
+        generic map (
+            width => 20
+        )
+        port map (
+            d  => key(i),
+            cl => cl,
+            q  => s_key(i)
+        );
+    end generate key_ch0;
+
+    sw_ch0: for i in 0 to 7 generate
+        chatter0: c_chatter
+        generic map (
+            width => 4
+        )
+        port map (
+            d  => sw(i),
+            cl => cl,
+            q  => s_sw(i)
+        );
+    end generate sw_ch0;
 end architecture rtl;
