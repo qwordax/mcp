c540be7a07d0912f0b74d1fbd0049414e9fed92e
Anatoly Nadolsky <a.nadolsky@proton.me>
2024-05-12 16:39:37 +0300

    `feat:` implement `e_mcp_cu_chs`

--- e_mcp_cu.vhd
+++ e_mcp_cu.vhd
@@ -64,6 +64,13 @@ begin
         p_q   => open
     );
 
+    l_chs: entity work.e_mcp_cu_chs
+    port map (
+        p_op  => p_op0,
+        p_cmd => p_cmd,
+        p_q   => open
+    );
+
     l_en: entity work.c_tff
     port map (
         p_r  => '0',
--- e_mcp_cu_chs.vhd
+++ e_mcp_cu_chs.vhd
@@ -2,29 +2,24 @@ library ieee;
 
 use ieee.std_logic_1164.all;
 
-entity e_mcp_cu_abs is
+entity e_mcp_cu_chs is
 port (
     p_op:  in  std_logic_vector(31 downto 0);
     p_cmd: in  std_logic_vector(37 downto 0);
     p_q:   out std_logic_vector(31 downto 0)
 );
-end entity e_mcp_cu_abs;
+end entity e_mcp_cu_chs;
 
-architecture rtl of e_mcp_cu_abs is
-    signal s_ci: std_logic;
-    signal s_op: std_logic_vector(31 downto 0);
+architecture rtl of e_mcp_cu_chs is
     signal s_sm: std_logic_vector(31 downto 0);
 begin
-    s_ci <= '1' when p_op(31) = '1' else '0';
-    s_op <= not p_op when s_ci = '1' else p_op;
-
     l_sm: entity work.c_sm
     generic map (
         g_width => 32
     )
     port map (
-        p_ci => s_ci,
-        p_a  => s_op,
+        p_ci => '1',
+        p_a  => not p_op,
         p_b  => (others => '0'),
         p_s  => s_sm,
         p_co => open
@@ -32,10 +27,10 @@ begin
 
     process (p_cmd) is
     begin
-        if p_cmd(18) = '1' then
+        if p_cmd(19) = '1' then
             p_q <= s_sm;
-        elsif p_cmd(29) = '1' then
-            p_q <= p_op and "01111111111111111111111111111111";
+        elsif p_cmd(24) = '1' then
+            p_q <= p_op xor "10000000000000000000000000000000";
         else
             p_q <= (others => '0');
         end if;
