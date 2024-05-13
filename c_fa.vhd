cb9967c62b298a667bb331f649e7b3a4ecdad73c
Anatoly Nadolsky <a.nadolsky@proton.me>
2024-05-13 18:30:17 +0300

    `fix:` fix `c_fa`

--- c_fa.vhd
+++ c_fa.vhd
@@ -7,29 +7,16 @@ port (
     p_ci: in  std_logic;
     p_a:  in  std_logic;
     p_b:  in  std_logic;
-    p_s:  out std_logic;
+    p_q:  out std_logic;
     p_co: out std_logic
 );
 end entity c_fa;
 
 architecture rtl of c_fa is
-    signal s_a: std_logic_vector(1 downto 0);
-    signal s_b: std_logic_vector(1 downto 0);
-    signal s_s: std_logic_vector(1 downto 0);
-    signal s_c: std_logic_vector(1 downto 0);
+    signal s_tmp: std_logic;
 begin
-    s_a(0) <= p_a;
-    s_b(0) <= p_b;
+    s_tmp <= p_a xor p_b;
 
-    s_s(0) <= s_a(0) xor s_b(0);
-    s_c(0) <= s_a(0) and s_b(0);
-
-    s_a(1) <= s_c(0);
-    s_b(1) <= p_ci;
-
-    s_s(1) <= s_a(1) xor s_b(1);
-    s_c(1) <= s_a(1) and s_b(1);
-
-    p_s  <= s_s(1);
-    p_co <= s_c(1);
+    p_q  <= p_ci xor s_tmp;
+    p_co <= (p_ci and s_tmp) or (p_a and p_b);
 end architecture rtl;
