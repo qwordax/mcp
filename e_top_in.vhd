045f6d5529217dc86d3d0fe9c497c9b7d30bb4cc
Anatoly Nadolsky <a.nadolsky@proton.me>
2024-06-01 20:12:49 +0300

    `fix:` update `e_top_in`

--- e_top_in.vhd
+++ e_top_in.vhd
@@ -6,6 +6,7 @@ entity e_top_in is
 port (
     p_r:  in  std_logic;
     p_s:  in  std_logic;
+    p_cl: in  std_logic;
     p_en: in  std_logic;
     p_q:  out std_logic_vector(31 downto 0);
     p_cs: out std_logic_vector(31 downto 0);
@@ -20,8 +21,8 @@ architecture rtl of e_top_in is
 
     signal s_dc_q: std_logic_vector(31 downto 0);
 
-    signal s_r: std_logic_vector(31 downto 0);
-    signal s_s: std_logic_vector(31 downto 0);
+    signal s_d: std_logic_vector(31 downto 0);
+    signal s_q: std_logic_vector(31 downto 0);
 begin
     s_ctr_r <= not p_en;
 
@@ -52,17 +53,19 @@ begin
     p_ml <= s_ctr_q(4);
 
     l_in: for i in 0 to 31 generate
-        s_r(i) <= p_r and s_dc_q(i);
-        s_s(i) <= p_s and s_dc_q(i);
+        s_d(i) <= '0' when p_r = '1' else
+                  '1' when p_s = '1' else '0';
 
         l_dff: entity work.c_dff
         port map (
-            p_r  => s_r(i),
-            p_s  => s_s(i),
-            p_d  => '0',
-            p_cl => '0',
-            p_en => '0',
-            p_q  => p_q(i)
+            p_r  => '0',
+            p_s  => '0',
+            p_d  => s_d(i),
+            p_cl => p_cl,
+            p_en => s_dc_q(i),
+            p_q  => s_q(i)
         );
     end generate l_in;
+
+    p_q <= s_q;
 end architecture rtl;
